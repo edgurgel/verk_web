8d404228b4c2b3ddc524391643adb8107905f277
Eduardo Gurgel <eduardo@gurgel.me>
2016-02-08 14:39:29 +1300

    Add coverex and prepare to publish on hex.pm

--- mix.exs
+++ mix.exs
@@ -1,39 +1,49 @@
 defmodule VerkWeb.Mixfile do
   use Mix.Project
 
+  @description """
+    A Verk dashboard
+  """
+
   def project do
     [app: :verk_web,
-     version: "0.0.1",
+     version: "0.9.0",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     test_coverage: [tool: Coverex.Task, coveralls: true],
+     name: "Verk Web",
+     description: @description,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [mod: {VerkWeb, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext, :verk]]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
   defp elixirc_paths(_),     do: ["lib", "web"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [{:phoenix, "~> 1.1.1"},
      {:phoenix_html, "~> 2.3"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:verk, "~> 0.9"},
      {:cowboy, "~> 1.0"},
+     {:phoenix_live_reload, "~> 1.0", only: :dev},
+     {:earmark, "~> 0.1.17", only: :docs},
+     {:ex_doc, "~> 0.8.0", only: :docs},
+     {:coverex, "~> 1.4.7", only: :test},
      {:meck, "~> 0.8", only: :test}]
+  end
+
+  defp package do
+    [ maintainers: ["Eduardo Gurgel Pinho", "Alisson Sales"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/edgurgel/verk_web"} ]
   end
 end
