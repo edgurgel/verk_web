0bb7b10f3f6b7b8898abfa47c013bd7b66e949c7
Eduardo Gurgel <eduardo@carnivallabs.com>
2016-02-10 14:31:14 +1300

    Bump version 0.9.1

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule VerkWeb.Mixfile do
 
   def project do
     [app: :verk_web,
-     version: "0.9.0",
+     version: "0.9.1",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
