ead089de57d041b54b4fb6d577561fc6832f6afe
Eduardo Gurgel <eduardo@carnivallabs.com>
2016-03-03 13:27:01 +1300

    Extract enqueued_at presentation to SharedView

--- web/views/shared_view.ex
+++ web/views/shared_view.ex
@@ -1,3 +1,8 @@
 defmodule VerkWeb.SharedView do
   use VerkWeb.Web, :view
+
+  def enqueued_at(nil), do: "N/A"
+  def enqueued_at(timestamp) do
+    timestamp |> Timex.Date.from(:secs) |> Timex.DateFormat.format!("{ISO}")
+  end
 end
