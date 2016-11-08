1b953f1213b4e0fcc54c209346ada612931ed0ea
Keyan Pishdadian <kpishdadian@gmail.com>
2016-11-08 02:02:33 -0500

    Show relative retry time in retries tab

--- web/views/retries_view.ex
+++ web/views/retries_view.ex
@@ -2,10 +2,11 @@ defmodule VerkWeb.RetriesView do
   use VerkWeb.Web, :view
 
   def jobs(failed_jobs) do
-    Enum.map failed_jobs, fn failed_job ->
+    Enum.map failed_jobs, fn {failed_job, score} ->
       %{
         jid: failed_job.jid,
         retry_count: failed_job.retry_count,
+        score: score |> Timex.from_unix |> Timex.format!("{relative}", :relative),
         class: failed_job.class,
         args: failed_job.args |> inspect,
         original_json: failed_job.original_json,
