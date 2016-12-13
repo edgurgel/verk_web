bcfba8a6d6871d1697104d6504cca78f69b63bd8
Alisson Sales <alisson.sales@gmail.com>
2016-12-13 15:40:54 +1300

    Fix test config
    Now we need to link verk_supervisor

--- config/test.exs
+++ config/test.exs
@@ -2,6 +2,9 @@ use Mix.Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
+config :verk_web,
+  link_verk_supervisor: true
+
 config :verk_web, VerkWeb.Endpoint,
   http: [port: 4001],
   server: false
