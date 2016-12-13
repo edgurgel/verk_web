52579e9e45a6f54e33b6400c23b2ff0391e3afe2
Alisson Sales <alisson.sales@gmail.com>
2016-12-13 14:24:54 +1300

    Add TrackingJobsHandler
    GenEvent used to aggregate stats from jobs finished and failed

--- lib/verk_web.ex
+++ lib/verk_web.ex
@@ -5,12 +5,14 @@ defmodule VerkWeb do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    children = [supervisor(VerkWeb.Endpoint, [])]
+    children = [supervisor(VerkWeb.Endpoint, []),
+                worker(Watcher, [Verk.EventManager, TrackingJobsHandler, []])]
     children = if Application.get_env(:verk_web, :link_verk_supervisor, false) do
       [supervisor(Verk.Supervisor, []) | children]
     else
       children
     end
+
     :ets.new(:verk_web_session, [:named_table, :public, read_concurrency: true])
     opts = [strategy: :one_for_one, name: VerkWeb.Supervisor]
     Supervisor.start_link(children, opts)
