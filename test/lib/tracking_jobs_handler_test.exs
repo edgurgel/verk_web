cfbc342fd55e1d7b07612337810d79be0a5308a2
Eduardo Gurgel <eduardo@gurgel.me>
2017-05-07 17:02:31 +1200

    Replace GenEvent with GenStage

--- test/lib/tracking_jobs_handler_test.exs
+++ test/lib/tracking_jobs_handler_test.exs
@@ -10,34 +10,33 @@ defmodule VerkWeb.TrackingJobsHandlerTest do
 
   describe "init/1" do
     test "returns correct initial state" do
-      pid          = self()
-      {:ok, state} = init(pid)
-      assert state == {pid, %{finished: 0, failed: 0}}
+      pid = self()
+      assert {:consumer, {^pid, %{finished: 0, failed: 0}}, subscribe_to: [Verk.EventProducer]} = init(pid)
     end
   end
 
-  describe "handle_event/2" do
+  describe "handle_events/2" do
     test "with job finished event" do
-      pid          = self()
-      {:ok, state} = init(pid)
+      pid = self()
+      {:consumer, state, subscribe_to: [Verk.EventProducer]} = init(pid)
 
-      assert handle_event(%Verk.Events.JobFinished{}, state) == {:ok, {pid, %{ finished: 1, failed: 0 }}}
-      assert handle_event(%Verk.Events.JobFinished{}, {pid, %{finished: 10, failed: 0}}) == {:ok, {pid, %{ finished: 11, failed: 0 }}}
+      assert handle_events([%Verk.Events.JobFinished{}], :from, state) == {:noreply, [], {pid, %{ finished: 1, failed: 0 }}}
+      assert handle_events([%Verk.Events.JobFinished{}], :from, {pid, %{finished: 10, failed: 0}}) == {:noreply, [], {pid, %{ finished: 11, failed: 0 }}}
     end
 
     test "with job failed event" do
-      pid          = self()
-      {:ok, state} = init(pid)
+      pid = self()
+      {:consumer, state, subscribe_to: [Verk.EventProducer]} = init(pid)
 
-      assert handle_event(%Verk.Events.JobFailed{}, state) == {:ok, {pid, %{ finished: 0, failed: 1 }}}
-      assert handle_event(%Verk.Events.JobFailed{}, {pid, %{finished: 0, failed: 10}}) == {:ok, {pid, %{ finished: 0, failed: 11 }}}
+      assert handle_events([%Verk.Events.JobFailed{}], :from, state) == {:noreply, [], {pid, %{ finished: 0, failed: 1 }}}
+      assert handle_events([%Verk.Events.JobFailed{}], :from, {pid, %{finished: 0, failed: 10}}) == {:noreply, [], {pid, %{ finished: 0, failed: 11 }}}
     end
 
     test "with unexpected event" do
-      pid          = self()
-      {:ok, state} = init(pid)
+      pid = self()
+      {:consumer, state, subscribe_to: [Verk.EventProducer]} = init(pid)
 
-      assert handle_event(:some_other_event, state) == {:ok, state}
+      assert handle_events([:some_other_event], :from, state) == {:noreply, [], state}
     end
   end
 
@@ -46,14 +45,14 @@ defmodule VerkWeb.TrackingJobsHandlerTest do
       pid   = self()
       state = {pid, %{finished: 1, failed: 2}}
 
-      {:ok, new_state} = handle_info(:broadcast_stats, state)
+      {:noreply, [], new_state} = handle_info(:broadcast_stats, state)
       assert new_state == {pid, %{finished: 0, failed: 0}}
       assert_receive {:stats, %{finished: 1, failed: 2}}
       assert_receive :broadcast_stats, 2000
     end
 
     test "unexpected info call" do
-      {:ok, state} = handle_info(:something, :state)
+      {:noreply, [], state} = handle_info(:something, :state)
       assert state == :state
     end
   end
