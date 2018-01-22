6e64e01901f90df2c2f03e2e3427a2fcef9daf01
Angelika Tyborska <atyborska93@gmail.com>
2018-01-22 20:49:50 +0100

    Show error backtrace in job modal

--- web/views/shared_view.ex
+++ web/views/shared_view.ex
@@ -5,4 +5,9 @@ defmodule VerkWeb.SharedView do
   def enqueued_at(timestamp) do
     timestamp |> round |> Timex.from_unix |> Timex.format!("{relative}", :relative)
   end
+
+  def error_backtrace(nil), do: "N/A"
+  def error_backtrace(string) do
+    Regex.replace(~r( {2,}), string, "")
+  end
 end
