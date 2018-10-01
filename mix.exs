3ef7e7205e00336186cc30ea4635d86d3f7cfa2b
Michael Stock <mikeastock@gmail.com>
2018-10-01 08:42:39 -0700

    Upgrade timex to 3.4.1
    https://github.com/bitwalker/timex/blob/3.4.1/CHANGELOG.md

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule VerkWeb.Mixfile do
      {:ex_doc, "~> 0.13", only: :dev},
      {:coverex, "~> 1.4", only: :test},
      {:meck, "~> 0.8", only: :test},
-     {:timex, "~> 3.3.0"}]
+     {:timex, "~> 3.4.1"}]
   end
 
   defp package do
